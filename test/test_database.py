b72b3a124c4074bb15d8d8677423ef4c9e8e1761
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-27 22:34:11 -0300

    feat: :white_check_mark: add test_dni_valido

--- test/test_database.py
+++ test/test_database.py
@@ -1,4 +1,5 @@
 import copy
+import helpers
 import unittest
 import database as db
 
@@ -7,40 +8,46 @@ class TestDataBase(unittest.TestCase):
 
     def setUp(self):
         db.Clientes.lista = [
-            db.Cliente(43416812, 'Martin', 'Ferreira'),
-            db.Cliente(20983645, 'Fernanda', 'Perez'),
-            db.Cliente(14239862, 'Ernesto', 'Hernandez'),
-            db.Cliente(20983512, 'Marcela', 'Lazo'),
+            db.Cliente('11111111', 'Martin', 'Ferreira'),
+            db.Cliente('20983645', 'Fernanda', 'Perez'),
+            db.Cliente('14239862', 'Ernesto', 'Hernandez'),
+            db.Cliente('20983512', 'Marcela', 'Lazo'),
         ]
 
     def test_buscar_cliente(self):
-        cliente_existente = db.Clientes.buscar_cliente(43416812)
-        cliente_inexistente = db.Clientes.buscar_cliente(99999)
+        cliente_existente = db.Clientes.buscar_cliente('11111111')
+        cliente_inexistente = db.Clientes.buscar_cliente('99999')
         self.assertIsNotNone(cliente_existente)
         self.assertIsNone(cliente_inexistente)
 
     def test_crear_cliente(self):
         cliente_nuevo = db.Clientes.crear_cliente(
-            98765432, 'Marcelo', 'Padilla')
+            '98765432', 'Marcelo', 'Padilla')
         self.assertEqual(len(db.Clientes.lista), 5)
-        self.assertEqual(cliente_nuevo.dni, 98765432)
+        self.assertEqual(cliente_nuevo.dni, '98765432')
         self.assertEqual(cliente_nuevo.nombre, 'Marcelo')
         self.assertEqual(cliente_nuevo.apellido, 'Padilla')
 
     def test_modificar_cliente(self):
-        cliente_a_modificar = copy.copy(db.Clientes.buscar_cliente(20983645))
+        cliente_a_modificar = copy.copy(db.Clientes.buscar_cliente('20983645'))
         cliente_modificado = db.Clientes.modificar_cliente(
-            20983645, 'Ferni', 'Bueno')
-        self.assertEqual(cliente_a_modificar.dni, 20983645)
+            '20983645', 'Ferni', 'Bueno')
+        self.assertEqual(cliente_a_modificar.dni, '20983645')
         self.assertEqual(cliente_a_modificar.nombre, 'Fernanda')
         self.assertEqual(cliente_a_modificar.apellido, 'Perez')
-        self.assertEqual(cliente_modificado.dni, 20983645)
+        self.assertEqual(cliente_modificado.dni, '20983645')
         self.assertEqual(cliente_modificado.nombre, 'Ferni')
         self.assertEqual(cliente_modificado.apellido, 'Bueno')
 
     def test_borrar_cliente(self):
-        cliente_borrado = db.Clientes.borrar_cliente(14239862)
-        cliente_buscado = db.Clientes.buscar_cliente(14239862)
+        cliente_borrado = db.Clientes.borrar_cliente('14239862')
+        cliente_buscado = db.Clientes.buscar_cliente('14239862')
         self.assertEqual(len(db.Clientes.lista), 3)
-        self.assertEqual(cliente_borrado.dni, 14239862)
+        self.assertEqual(cliente_borrado.dni, '14239862')
         self.assertIsNone(cliente_buscado)
+
+    def test_dni_valido(self):
+        self.assertTrue(helpers.dni_valido('99999999', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('11111111', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('123', db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('23fds', db.Clientes.lista))
