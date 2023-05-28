c1238df0ea3c71274f1a183350c58aacbe702468
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-28 10:14:33 -0300

    fix: :bug: fix

--- helpers.py
+++ helpers.py
@@ -12,14 +12,14 @@ def limpiar_consola():
 def leer_texto(long_min: int = 0, long_max: int = 100, mensaje: str = None):
     print(mensaje) if mensaje else None
     while True:
-        texto: str = input('>')
+        texto: str = input('> ')
         if long_min <= len(texto) <= long_max:
             return texto
 
 
 def dni_valido(dni: str, lista: List[Cliente]):
-    if not re.match('[0-9]{8}', dni):
-        print('DNI incorrecto')
+    if not re.match('[0-9]{8}$', dni):
+        print(f'DNI {dni} incorrecto!')
         return False
     for cliente in lista:
         if cliente.dni == dni:
